8e5d6c3748fb46f3a1c66c2e44c4d4fdd5ec5c8d
raimundocorrea <rjcorrea@miuandes.cl>
2018-09-30 21:11:07 -0300

    clase bitmon sin parametros

---
+++ Bitmons_Entrega2/Bitmons_Entrega2/Bitmon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bitmons_Entrega2
+{
+    class Bitmon
+    {
+        string nombre;
+        int stamina;
+        int vida;
+        int naturaleza;
+        int AttackBase;
+        int DefBase;
+        public bool estaActivo = false;
+
+        public Bitmon(string nombre, int stamina, int vida, int naturaleza, int attack, int def)
+        {
+            this.nombre = nombre;
+            this.stamina = stamina;
+            this.vida = vida;
+            this.naturaleza = naturaleza;
+            AttackBase = attack;
+            DefBase = def;
+        }
+    }
+}
