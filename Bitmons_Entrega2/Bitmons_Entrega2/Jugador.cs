a26ece28bb622dc006b0fb3adc4ed4eef5c8ae4a
sigalaz <sigalaz@miuandes.cl>
2018-10-03 18:16:59 -0400

    Edicion Clase Jugador

--- Bitmons_Entrega2/Bitmons_Entrega2/Jugador.cs
+++ Bitmons_Entrega2/Bitmons_Entrega2/Jugador.cs
@@ -10,8 +10,9 @@ namespace Bitmons_Entrega2
     {
         public string nombre;
         List<Bitmon> Bitmoms;
-        int jugada = 0;
-
+        int jugada ;
+        int tipoAtaque;
+        bool EsTuTurno = false;
         public Jugador(string nombre)
         {
             this.nombre = nombre;
@@ -30,19 +31,26 @@ namespace Bitmons_Entrega2
             }
         }
 
-        private void verRival()
+                
+        public void Estrategia()    // Es el metodo que permite al jugador elegir que hacer en el juego
         {
+            bool error = true;
+            Console.WriteLine("Que movimiento deseas hacer {0}?:\n [1] Cambiar Bitmom activo \n [2] Atacar \n [3] Ver info Bitmoms ");
+            
+            while(error){
+            try{
+                jugada = Convert.ToInt32(Console.Read());
+                error = false;
+                }
+            catch (Exception e){
+                Console.WriteLine(e.GetType);
+                }
 
-        }
 
-        private void verBitmons()
-        {
 
-        }
+            }
 
-        public void Estrategia(int e)
-        {
-
+            
         }
     }
 }
