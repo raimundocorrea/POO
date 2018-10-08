09bb2a86b010023ee1aaf8953910197968d4aeb7
raimundocorrea <rjcorrea@miuandes.cl>
2018-10-08 20:34:26 -0300

    codigo avanzado

---
+++ Entrega2_POO/Especial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2_POO
+{
+    class Especial : Naturaleza
+    {
+        protected override int Attack(int ataqueBase, Habilidad habilidad)
+        {
+            return 1;
+        }
+
+        protected override void Habilility()
+        {
+
+        }
+    }
+}
---
+++ Entrega2_POO/Naturaleza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2_POO
+{
+    abstract class Naturaleza
+    {
+        public List<Habilidad> habilidades;
+
+        protected abstract int Attack(int ataqueBase, Habilidad habilidad);
+
+        protected abstract void Habilility();
+
+        public void getPoderes(Naturaleza n)
+        {
+            foreach (Habilidad h in habilidades)
+            {
+                if (h.naturaleza == n)
+                {
+                    h.verHabilidad();
+                }
+            }
+        }
+    }
+}
